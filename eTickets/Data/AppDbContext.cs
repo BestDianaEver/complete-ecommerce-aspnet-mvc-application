39e21d67de2084d00b452a4fc756b47c456c0d74
Diana <diana.pelyte@gmail.com>
2024-02-26 10:16:51 +0000

    Added order and order items and search, updated database

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -28,5 +28,9 @@ namespace eTickets.Data
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
 
+
+        //Orders related tables
+        public DbSet<Order> Orders {  get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
