453bda116e7f1a9a6a28082ff954e61c09d89f94
Diana <diana.pelyte@gmail.com>
2024-02-26 15:41:04 +0000

    Shopping cart summary view, orders controller added

---
+++ eTickets/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using eTickets.Data.Cart;
+using eTickets.Data.Services;
+using eTickets.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMoviesService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IMoviesService moviesService, ShoppingCart shoppingCart)
+        {
+            _moviesService = moviesService;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+    }
+}
