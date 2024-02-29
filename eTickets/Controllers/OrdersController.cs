d18a8544bde3704bbe8dc4cbc22a1fcb89685554
Diana <diana.pelyte@gmail.com>
2024-02-29 12:53:37 +0000

    added orders list

--- eTickets/Controllers/OrdersController.cs
+++ eTickets/Controllers/OrdersController.cs
@@ -18,6 +18,14 @@ namespace eTickets.Controllers
             _ordersService = ordersService;
         }
 
+        public async Task<IActionResult> Index()
+        {
+            string userId = "";
+
+            var orders = await _ordersService.GetOrdersByUserIdAsync(userId);
+            return View(orders);
+        }
+
         public IActionResult ShoppingCart()
         {
             var items = _shoppingCart.GetShoppingCartItems();
