fdaa8544b50ff63474d181f6274576bf903eaf10
Diana <diana.pelyte@gmail.com>
2024-03-11 11:39:06 +0000

    Users list added, _identity updated, updated orders to include roles

--- eTickets/Models/Order.cs
+++ eTickets/Models/Order.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eTickets.Models
 {
@@ -9,6 +10,8 @@ namespace eTickets.Models
 
         public string Email { get; set; }
         public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
 
         public List<OrderItem> OrderItems { get; set; }
     }
